e345f63af4e9be835c01c8da61739630f70ec714
Irfan Khan <irfan.infinitybits@gmail.com>
2023-04-03 10:43:33 +0500

    1. multer file too large error resolved

--- src/middlewares/uploadData.js
+++ src/middlewares/uploadData.js
@@ -24,51 +24,10 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 1024 * 1024,
+    fileSize: 1024 * 1024 * 30,
     fields: 5
   },
   fileFilter: fileFilter
 });
 
 module.exports = upload;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const multer = require("multer");
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "./src/uploads/");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, `${Date.now()}-${file.originalname}`);
-//   },
-// });
-
-// const fileFilter = (req, file, cb) => {
-//   if (file.mimetype === "video/mp4" || file.mimetype === "video/mkv") {
-//     cb(null, true);
-//   } else {
-//     cb(null, false);
-//   }
-// };
-
-// const upload = multer({
-//   storage: storage,
-//   limits: { fieldSize: 1024 * 1024 },
-//   fileFilter: fileFilter,
-// });
-
-// module.exports = upload;
