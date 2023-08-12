da63bedc479987946ed2e7f8e99ac666d0a5dbaf
shubham ghanghotia <129264144+softgenicsShubham@users.noreply.github.com>
2023-08-12 18:07:29 +0530

    last update 12 aug 2023

--- src/controllers/version 2.0/country.js
+++ src/controllers/version 2.0/country.js
@@ -1,33 +1,47 @@
 const logger = require("../../utils/logger")
-const {Country} = require('../../models')
-const {City} = require('../../models')
+const { Country } = require('../../models')
+const { City } = require('../../models')
+const errorHandling = require("../../utils/errorObject")
 
 
 
-const getAllcountries = async (req, res, next)=>{
-logger.info("getting all country ")
-
-const countries = await Country.findAll()
-// console.log(countries)
-res.json(countries)
+const getAllcountries = async (req, res, next) => {
+    logger.info('INFO -> GETTING ALL COUNTRY DETAILS API CALLED')
+    try {
+        let country = await Country.findAll({})
+        if (!country) throw errorHandling('there is not no country in the db')
+        country = JSON.parse(JSON.stringify(country))
+        res.status(200).json({
+            message: 'successfull',
+            payload: country
+        })
+    } catch (error) {
+        logger.error(error)
+        res.status(500).json({ message: 'error while geting the country details, Please try again after sometime' })
+    }
 }
 
 
 
 
-const getCitiesbyCode = async(req, res, next)=>{
-    logger.info("getitng cities by country code")
-
-    const country_code = req.query.q;
-
-    
-    const cities = await City.findAll({
-        where: {
-            country_code: country_code
-          }  
-    })
-    res.json(cities)    
-
+const getCitiesbyCode = async (req, res, next) => {
+    logger.info('INFO -> GETTING CITIES BY COUNTRY CODE API CALLED')
+    try {
+        const { country_code } = req.params;
+       
+        let cities = await City.findAll({
+            where: { country_code }
+        })
+        if (!cities) throw errorHandling('not found')
+        cities = JSON.parse(JSON.stringify(cities))
+        res.status(200).json({
+            message: 'successfull',
+            payload: cities
+        })
+    } catch (error) {
+        logger.error(error)
+        res.status(500).json({ message: 'error while getting the cities details, Please try again after some time' })
+    }
 }
 
 
--- src/routes/country.js
+++ src/routes/country.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 
-const {getAllcountries, getCitiesbyCode} = require('../controllers/version 2.0/country')
+const countryApi = require('../controllers/version 2.0/country')
 
-router.get('/allCountry', getAllcountries)
-router.get('/getCitiesByCode', getCitiesbyCode)
+router.get('/allCountry', countryApi.getAllcountries)
+router.get('/getCitiesByCode/:country_code', countryApi.getCitiesbyCode)
 
 module.exports = router
